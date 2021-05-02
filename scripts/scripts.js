06179f5d5afbae2b004267d9d3b515c913d9086f
Felipe Augusto <augusto.felipao22@gmail.com>
2021-05-02 11:13:23 -0300

    Validação de tarefa
    Adição de validação nas tarefas, para verificar se entrada de dados é null ou vazia..

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,15 +3,21 @@ let idTarefa = 0;
 function addTarefa(){
     const leitura = prompt("Entre com a tarefa que deseja adicionar: ", "Exemplo: Lavar louça mais tarde.");
     
-    const container = document.getElementById("tarefas");
+    if(leitura != null){
+        if(!leitura.trim()){
+            alert("Tarefa inválida !");
+        }else{
+            const container = document.getElementById("tarefas");
 
-    const tarefa = `${container.innerHTML} 
-    <div id="tarefa-${idTarefa}">
-        <p><input type="checkbox" onclick="removeTarefa(${idTarefa})"> ${leitura}</p>
-    </div>`;
+            const tarefa = `${container.innerHTML} 
+            <div id="tarefa-${idTarefa}">
+                <p><input type="checkbox" onclick="removeTarefa(${idTarefa})"> ${leitura}</p>
+            </div>`;
 
-    container.innerHTML = tarefa;
-    idTarefa++;
+            container.innerHTML = tarefa;
+            idTarefa++;
+        }
+    }
 }
 
 function removeTarefa(number){
