210f76c41ccafb42f70b4c1f10c51aeb2398289e
Felipe Augusto <augusto.felipao22@gmail.com>
2021-05-01 21:46:09 -0300

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+let idTarefa = 0;
+
+function addTarefa(){
+    const leitura = prompt("Entre com a tarefa que deseja adicionar: ", "Exemplo: Lavar louça mais tarde.");
+    
+    const container = document.getElementById("tarefas");
+
+    const tarefa = `${container.innerHTML} 
+    <div id="tarefa-${idTarefa}">
+        <p><input type="checkbox" onclick="removeTarefa(${idTarefa})"> ${leitura}</p>
+    </div>`;
+
+    container.innerHTML = tarefa;
+    idTarefa++;
+}
+
+function removeTarefa(number){
+    const remove = document.getElementById(`tarefa-${number}`);
+    const texto = remove.innerText;
+
+    remove.innerHTML = `<s><p><input type="checkbox" disabled="disabled" checked> ${texto}</p></s>`
+}
